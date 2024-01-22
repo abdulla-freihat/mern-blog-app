e6db5511ee5be3a8af56cf794e446f31c9d624db
abdulla freihat <abdulla_freihat@hotmail.com>
2024-01-21 22:27:20 -0500

    connect to mongo db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,21 @@
+
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config({path:__dirname+'/.env'});
+
+
+
+const db = process.env.MONGO_URL;
+console.log(db)
+
+mongoose.connect(db)
+.then(()=>{
+
+           console.log('mongo db is connected')
+}).catch(err =>{
+         console.log(err);
+})
 
 
 
@@ -6,7 +23,7 @@ const app = express();
 
 
 
-app.listen(8000 , ()=>{
+app.listen(process.env.PORT, ()=>{
 
         console.log('server is running in server 8000')
 })
